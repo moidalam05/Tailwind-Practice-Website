a3da085e9fc19443dc2c99404d41932f8302d6df
moidalam05 <moidalam005@hotmail.com>
2024-04-04 15:47:16 +0530

    faq design complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const navDialog = document.getElementById('nav-dialog');
 
 // Navbar toggerler
 function handleMenu() {
-    navDialog.classList.toggle('hidden');
+	navDialog.classList.toggle('hidden');
 }
 
 // companies logo slider
@@ -11,28 +11,29 @@ const initialTranslateLTR = -48 * 4;
 const initialTranslateRTL = 36 * 4;
 
 function setupIntersectionObserver(element, isLTR, speed) {
-    const intersectionCallback = (entries) => { 
-        const isIntersecting = entries[0].isIntersecting;
-        if(isIntersecting) {
-            document.addEventListener('scroll',scrollHandler);
-        } else {
-            document.removeEventListener('scroll',scrollHandler);
-        }
-    }
-    const interserctionObserver = new IntersectionObserver(intersectionCallback);
-    interserctionObserver.observe(element);
+	const intersectionCallback = (entries) => {
+		const isIntersecting = entries[0].isIntersecting;
+		if (isIntersecting) {
+			document.addEventListener('scroll', scrollHandler);
+		} else {
+			document.removeEventListener('scroll', scrollHandler);
+		}
+	};
+	const interserctionObserver = new IntersectionObserver(intersectionCallback);
+	interserctionObserver.observe(element);
 
-    function scrollHandler() { 
-        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+	function scrollHandler() {
+		const translateX =
+			(window.innerHeight - element.getBoundingClientRect().top) * speed;
 
-        let totalTranslate = 0;
-        if (isLTR) { 
-            totalTranslate = initialTranslateLTR + translateX;
-        } else {
-            totalTranslate = -(initialTranslateRTL + translateX);
-        }
-        element.style.transform = `translateX(${totalTranslate}px)`
-    }
+		let totalTranslate = 0;
+		if (isLTR) {
+			totalTranslate = initialTranslateLTR + translateX;
+		} else {
+			totalTranslate = -(initialTranslateRTL + translateX);
+		}
+		element.style.transform = `translateX(${totalTranslate}px)`;
+	}
 }
 
 const line1 = document.getElementById('line1');
@@ -44,3 +45,16 @@ setupIntersectionObserver(line1, true, 0.15);
 setupIntersectionObserver(line2, false, 0.15);
 setupIntersectionObserver(line3, true, 0.15);
 setupIntersectionObserver(line4, true, 0.8);
+
+// faq accordion
+const dtElements = document.querySelectorAll('dt');
+dtElements.forEach((element) => {
+	element.addEventListener('click', () => {
+		const ddId = element.getAttribute('aria-controls');
+		const ddElement = document.getElementById(ddId);
+		const ddArrowIcon = element.querySelectorAll('i')[0];
+
+		ddElement.classList.toggle('hidden');
+		ddArrowIcon.classList.toggle('-rotate-180');
+	});
+});
