ed48128c9a208df0ede1c8d4e656c4876d70b282
moidalam05 <moidalam005@hotmail.com>
2024-04-04 05:57:46 +0530

    companies scroll slider desing complete

--- index.js
+++ index.js
@@ -4,3 +4,41 @@ const navDialog = document.getElementById('nav-dialog');
 function handleMenu() {
     navDialog.classList.toggle('hidden');
 }
+
+// companies logo slider
+
+const initialTranslateLTR = -48 * 4;
+const initialTranslateRTL = 36 * 4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+    const intersectionCallback = (entries) => { 
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting) {
+            document.addEventListener('scroll',scrollHandler);
+        } else {
+            document.removeEventListener('scroll',scrollHandler);
+        }
+    }
+    const interserctionObserver = new IntersectionObserver(intersectionCallback);
+    interserctionObserver.observe(element);
+
+    function scrollHandler() { 
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if (isLTR) { 
+            totalTranslate = initialTranslateLTR + translateX;
+        } else {
+            totalTranslate = -(initialTranslateRTL + translateX);
+        }
+        element.style.transform = `translateX(${totalTranslate}px)`
+    }
+}
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
